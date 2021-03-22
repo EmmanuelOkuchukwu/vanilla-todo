e1c2d54e726568635e6a5a8695cbb2390b8451e2
emmanz95 <emmanza2@gmail.com>
2021-03-22 12:40:26 +0000

    inital commit

---
+++ app/js/script.js
@@ -0,0 +1,5 @@
+const userName = "Emmanuel";
+const todoLists = ['Organize office', 'Read Bible Plan'];
+const displayingUsers = todoLists.map((todoList) => {
+    console.log(todoList)
+});
---
+++ dist/script.js
@@ -0,0 +1,2 @@
+const userName="Emmanuel",todoLists=["Organize office","Read Bible Plan"],displayingUsers=todoLists.map((s=>{console.log(s)}));
+//# sourceMappingURL=script.js.map
